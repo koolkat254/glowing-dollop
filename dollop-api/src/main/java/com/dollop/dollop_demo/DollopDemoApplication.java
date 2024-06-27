d9fc4bc3c94cc1fd0ff599a776b6352a2b50c642
koolkat254 <75220971+koolkat254@users.noreply.github.com>
2024-06-27 18:49:55 -0500

    added api

---
+++ dollop-api/src/main/java/com/dollop/dollop_demo/DollopDemoApplication.java
@@ -0,0 +1,13 @@
+package com.dollop.dollop_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DollopDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DollopDemoApplication.class, args);
+	}
+
+}
---
+++ dollop-api/src/test/java/com/dollop/dollop_demo/DollopDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dollop.dollop_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DollopDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
